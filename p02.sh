4510b76073d8cefe9905faef9b7aed53dbbc80ec
Peter Dick PC_VM_L <pdick4@fh-bielefeld.de>
2016-04-20 00:27:23 +0200

    Fehler in Aufgabe 1 gefixt

--- p02.sh
+++ p02.sh
@@ -3,12 +3,12 @@ dateiexistiert() {
   file="$1"
   if [ -e "$file" ] 
     then
-      echo "Die Datei $file existiert"
+      echo "Die Datei $file existiert."
   elif [ -d "$file" ]
     then
-      echo "Das Verzeichnis $file existiert"
+      echo "Das Verzeichnis $file existiert."
   else
-    echo "$file existiert nicht"
+    echo "$file existiert nicht."
   fi
 }
 
@@ -16,12 +16,12 @@ dateiregular() {
   file="$1"
   if [ -f "$file" ] 
     then
-      echo "$file ist eine regulaere Datei"
+      echo "$file ist eine regulaere Datei."
   elif [ -d "$file" ]
     then
-      echo "$file ist ein Verzeichnis"
+      echo "$file ist ein Verzeichnis."
   else
-    echo "$file ist weder eine regulaere Datei oder ein Verzeichnis" 
+    echo "$file ist weder eine regulaere Datei oder ein Verzeichnis." 
   fi
 }
 
@@ -29,9 +29,9 @@ symblink() {
   file="$1"
   if [ -h "$file" ] 
     then
-      echo "$file ist ein symbolischer Link"
+      echo "$file ist ein symbolischer Link."
   else
-    echo "$file ist kein symbolischer Link"
+    echo "$file ist kein symbolischer Link."
   fi
 }
 
@@ -39,16 +39,21 @@ aufruferbesitzer() {
   file="$1"
   if [ -O "$file" ] 
     then
-      echo "der Aufrufer besitzt die Datei $file"
+      echo "der Aufrufer besitzt die Datei $file."
   else
-    echo "der Aufrufer besitzt die Datei $file nicht"
+    echo "der Aufrufer besitzt die Datei $file nicht."
   fi
 }
 
 besitzer() {
   file="$1"
-  user=$(ls -la  | grep $file | cut -d" " -f4)
-  echo "$user besitzt die Datei"
+  if [ -e "$file" ]
+    then
+      user=$(ls -la  | grep $file | cut -d" " -f4)
+      echo "$user besitzt die Datei."
+    else
+      echo "Datei existiert nicht."
+  fi
 }
 
 dateiexistiert $1
